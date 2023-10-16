24fc02d09e342f2b2d041b8513df705f6eb9a01d
BananaF1x <brovkoman@gmail.com>
2023-10-16 21:55:43 +0300

    Додав унарні плюси

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let firstNumber = prompt("Введіть перше число");
-let secondNumber = prompt("Введіть друге число");
-let thirdNumber = prompt("Введіть третє число");
+let firstNumber = +prompt("Введіть перше число");
+let secondNumber = +prompt("Введіть друге число");
+let thirdNumber = +prompt("Введіть третє число");
 alert(`Середнє значення чисел = ${(firstNumber + secondNumber + thirdNumber)/3}`);
